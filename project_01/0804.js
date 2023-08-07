1fc86423aa301050abe39446e6021f69f6657bd5
jjsh03 <aberrant03@gmail.com>
2023-08-07 14:03:08 +0900

    study: 개별프로젝트-01 ing

--- project_01/0804.js
+++ project_01/0804.js
@@ -1,10 +1,20 @@
-$('.enter').on('click', function () {
-  let content = document.querySelector('#remindContent').value;
-  let newNote = document.createElement('li');
-  let reminder = document.querySelector('.reminderList');
+let inputBox = document.getElementById('inputField');
+let addToDo = document.getElementById('addToDo');
+let toDoList = document.getElementById('toDoList');
 
-  newNote.innerHTML = content;
-  reminder.append(newNote);
+addToDo.addEventListener('click', function () {
+  var list = document.createElement('li');
+  if (!inputBox.value) alert('내용을 입력해 주세요!');
+  else {
+    list.innerText = inputBox.value;
+    toDoList.appendChild(list);
+    inputBox.value = '';
+  }
 
-  content = '';
+  list.addEventListener('click', function () {
+    list.style.textDecoration = 'line-through';
+  });
+  list.addEventListener('dblclick', function () {
+    toDoList.removeChild(list);
+  });
 });
